590bdad00523eb3c03dfe75cdb44164037388688
Nikolas Andreou <11696956+Codeh4ck@users.noreply.github.com>
2022-04-10 20:12:46 +0300

    Added RoleValidator (interface & concrete type) which determines if a model which implements IHasRole can execute a request

---
+++ Codelux.ServiceStack/Roles/IRoleValidator.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Codelux.Common.Models;
+using Microsoft.Extensions.Primitives;
+
+namespace Codelux.ServiceStack.Roles
+{
+    public interface IRoleValidator
+    {
+        bool HasRole(IHasRole model, int roleLevel);
+        Task<IHasRole> GetModelAsync(object key, CancellationChangeToken token = default);
+    }
+}
---
+++ Codelux.ServiceStack/Roles/RoleValidatorBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Codelux.Common.Models;
+using Microsoft.Extensions.Primitives;
+
+namespace Codelux.ServiceStack.Roles
+{
+    public abstract class RoleValidatorBase : IRoleValidator
+    {
+        public bool HasRole(IHasRole model, int roleLevel)
+        {
+            if (model.Roles == null || model.Roles.Count == 0) return false;
+
+            IRole highestRole = model.Roles.OrderByDescending(x => x.Level).FirstOrDefault();
+            if (highestRole == null) return false;
+
+            return highestRole.Level >= roleLevel;
+        }
+
+        public abstract Task<IHasRole> GetModelAsync(object key, CancellationChangeToken token = default);
+    }
+}
