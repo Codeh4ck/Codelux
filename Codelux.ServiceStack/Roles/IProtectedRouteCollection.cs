30b87d708a0a766f3e5379155139cb14e6285dae
Nikolas Andreou <11696956+Codeh4ck@users.noreply.github.com>
2022-04-10 20:12:07 +0300

    Added ProtectedRouteCollection interface & concrete type which holds protected route data (request DTO & minimum required role level)

---
+++ Codelux.ServiceStack/Roles/IProtectedRouteCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Codelux.Common.Models;
+
+namespace Codelux.ServiceStack.Roles
+{
+    public interface IProtectedRouteCollection
+    {
+        bool AddProtectedRoute(Type requestType, IRole role);
+        bool RemoveProtectedRoute(Type requestType);
+        bool IsProtectedRequestType(Type requestType);
+        bool CanExecute(Type requestType, IHasRole role);
+    }
+}
