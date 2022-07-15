3990c8b4c25f03deabe3103e4829197e395a71bf
Nikolas <nickandreou94@gmail.com>
2022-07-15 12:19:18 +0300

    Created password generator interface and concrete implementation

---
+++ Codelux/Utilities/IPasswordGenerator.cs
@@ -0,0 +1,7 @@
+﻿namespace Codelux.Utilities
+{
+    public interface IPasswordGenerator
+    {
+        string GeneratePassword(int length, bool capitals = true, bool numbers = false, bool symbols = false);
+    }
+}
---
+++ Codelux/Utilities/PasswordGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Codelux.Utilities
+{
+    public class PasswordGenerator : IPasswordGenerator
+    {
+        private static readonly Random Random = new Random(DateTime.Now.Millisecond);
+
+        public string GeneratePassword(int length, bool capitals = true, bool numbers = false, bool symbols = false)
+        {
+            string initialPool = "abcdefghijklmnopqrstuvwxyz";
+
+            StringBuilder charPoolBuilder = new StringBuilder();
+
+            charPoolBuilder.Append(initialPool);
+
+            if (capitals)
+                charPoolBuilder.Append(initialPool.ToUpper());
+
+            if (numbers)
+                charPoolBuilder.Append("0123456789");
+
+            if (symbols)
+                charPoolBuilder.Append("!@#$%^&*()_+-=[]{};:,./<>?");
+
+            string charPool = charPoolBuilder.ToString();
+
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            for (int x = 0; x < length; x++)
+                passwordBuilder.Append(charPool[Random.Next(0, charPool.Length - 1)]);
+
+            return passwordBuilder.ToString();
+        }
+    }
+}
