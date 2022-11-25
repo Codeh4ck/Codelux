ef8ee93d436880cfec9f722b0c31dd3117116f11
Nikolas Andreou <nickandreou94@gmail.com>
2022-11-25 17:21:04 +0200

    Add AsyncMapperBase that supports asynchronous mapping between models

---
+++ Codelux/Mappers/AsyncMapperBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Codelux.Mappers
+{
+    public abstract class AsyncMapperBase<TIn, TOut> : MapperBase<TIn, TOut>
+    {
+        public Task<TOut> MapAsync(TIn model, CancellationToken token = default) => Task.Factory.StartNew(() => Map(model), token);
+        public Task<IEnumerable<TOut>> MapAsync(IEnumerable<TIn> models, CancellationToken token = default)
+            => Task.Factory.StartNew(() => Map(models), token);
+    }
+}
