bd027b22a66ed294bd7361a7463045ca0332d10a
Nikolas Andreou <nickandreou94@gmail.com>
2022-11-27 00:23:38 +0200

    Adhere to Microsoft's naming standards

--- Codelux.Common/Models/ServiceCodes.cs
+++ Codelux.Common/Models/ServiceCodes.cs
@@ -2,9 +2,9 @@
 {
     public static class ServiceCodes
     {
-        public const int SUCCESS = 0;
-        public const int VALIDATION_ERROR = -1;
-        public const int UNAUTHORISED_REQUEST = -2;
-        public const int INTERNAL_SERVER_ERROR = -3;
+        public const int Success = 0;
+        public const int ValidationError = -1;
+        public const int UnauthorisedRequest = -2;
+        public const int InternalServerError = -3;
     }
 }
