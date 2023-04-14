d6cde8f34b346791adcce7424ab7e197539d961b
Nikolas Andreou <nickandreou94@gmail.com>
2023-04-14 18:37:39 +0300

    Add fire and forget task extension method

---
+++ Codelux.Common/Extensions/TaskExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace Codelux.Common.Extensions;
+
+public static class TaskExtensions
+{
+    /// <summary>
+    /// Runs a task as fire-and-forget. Observes the task to prevent the UnobservedTaskException event from raising.
+    /// </summary>
+    /// <param name="task">The task to be forgotten.</param>
+    /// <param name="continueOnCapturedContext">Instructs the task awaiter to attempt continuation on the captured context or not.</param>
+    public static void Forget(this Task task, bool continueOnCapturedContext = true)
+    {
+        if (!task.IsCompleted || task.IsFaulted)
+        {
+            _ = ForgetAwaited(task, continueOnCapturedContext);
+        }
+
+        static async Task ForgetAwaited(Task task, bool continueOnCapturedContext)
+        {
+            try
+            {
+                await task.ConfigureAwait(continueOnCapturedContext);
+            } catch
+            {
+                // Nothing to do here
+            }
+        }
+    }
+}
