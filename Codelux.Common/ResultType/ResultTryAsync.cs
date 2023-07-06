094cbf2a52bf701d8ba0cda642fc6d3002280d52
Nikolas Andreou <nickandreou94@gmail.com>
2023-07-06 08:43:51 +0300

    Create Result async helper methods

---
+++ Codelux.Common/ResultType/ResultTryAsync.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Codelux.Common.ResultType;
+
+public static partial class Result
+{
+    public static Task<IResult<TOk, Exception>> TryAsync<TOk>(Action action, TOk ok)
+    {
+        try
+        {
+            action();
+            return Task.FromResult(Ok<TOk, Exception>(ok));
+        }
+        catch (Exception exn)
+        {
+            return Task.FromResult(Fail(ok, exn));
+        }
+    }
+
+    public static async Task<IResult<TOk, TFail>> TryAsync<TOk, TFail>(Func<Task<TOk>> fnOk, TFail fail)
+    {
+        try
+        {
+            return Ok(await fnOk(), fail);
+        }
+        catch
+        {
+            return Fail<TOk, TFail>(fail);
+        }
+    }
+
+    public static async Task<IResult<TOk, TFail>> TryAsync<TOk, TFail>(Func<Task<TOk>> fnOk, Func<Task<TFail>> fnFail)
+    {
+        try
+        {
+            return Ok<TOk, TFail>(await fnOk());
+        }
+        catch
+        {
+            return Fail<TOk, TFail>(await fnFail());
+        }
+    }
+
+    public static Task<IResult<TOk, TFail>> TryAsync<TOk, TFail>(Action action, TOk ok, TFail fail)
+    {
+        try
+        {
+            action();
+            return Task.FromResult(Ok(ok, fail));
+        }
+        catch
+        {
+            return Task.FromResult(Fail(ok, fail));
+        }
+    }
+
+    public static async Task<IResult<TOk, TFail>> TryAsync<TOk, TFail>(Action action, Func<Task<TOk>> fnOk,
+        Func<Task<TFail>> fnFail)
+    {
+        try
+        {
+            action();
+            return Ok<TOk, TFail>(await fnOk());
+        }
+        catch
+        {
+            return Fail<TOk, TFail>(await fnFail());
+        }
+    }
+}
---
+++ Codelux.Common/ResultType/ResultTryCatchAsync.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Codelux.Common.ResultType;
+
+public static partial class Result
+{
+    public static Task<IResult<TOk, TFail>> TryCatchAsync<TOk, TFail, TException>(Action action, TOk ok, TFail fail)
+        where TException : Exception
+    {
+        try
+        {
+            action();
+            return Task.FromResult(Ok(ok, fail));
+        }
+        catch (TException)
+        {
+            return Task.FromResult(Fail(ok, fail));
+        }
+    }
+
+    public static async Task<IResult<TOk, TFail>> TryCatchAsync<TOk, TFail, TException>(Action action,
+        Func<Task<TOk>> fnOk, Func<Task<TFail>> fnFail) where TException : Exception
+    {
+        try
+        {
+            action();
+            return Ok<TOk, TFail>(await fnOk());
+        }
+        catch (TException)
+        {
+            return Fail<TOk, TFail>(await fnFail());
+        }
+    }
+
+    public static async Task<IResult<TOk, TFail>> TryCatchAsync<TOk, TFail, TException>(Func<Task<TOk>> fnOk,
+        TFail fail) where TException : Exception
+    {
+        try
+        {
+            return Ok(await fnOk(), fail);
+        }
+        catch (TException)
+        {
+            return Fail<TOk, TFail>(fail);
+        }
+    }
+
+    public static async Task<IResult<TOk, TFail>> TryCatchAsync<TOk, TFail, TException>(Func<Task<TOk>> fnOk,
+        Func<Task<TFail>> fnFail) where TException : Exception
+    {
+        try
+        {
+            return Ok<TOk, TFail>(await fnOk());
+        }
+        catch (TException)
+        {
+            return Fail<TOk, TFail>(await fnFail());
+        }
+    }
+}
