fffe4659a3c84b04e3c51f0e1412b4d1e254febd
Nikolas Andreou <nickandreou94@gmail.com>
2023-07-06 09:04:09 +0300

    Add test Ok/Fail result models

---
+++ Codelux.Tests/ResultType/ResultTestModels.cs
@@ -0,0 +1,23 @@
+﻿namespace Codelux.Tests.ResultType;
+
+public class TestOkResult
+{
+    private static TestOkResult _instance;
+    public static TestOkResult Instance => _instance ??= new(); 
+    
+    public static TestOkResult Create() => new();
+    
+    public string Message => "This is an Ok result";
+    public bool Success = true;
+}
+
+public class TestFailResult
+{
+    private static TestFailResult _instance;
+    public static TestFailResult Instance => _instance ??= new(); 
+    
+    public static TestFailResult Create() => new();
+    
+    public string Message => "This is a Fail result";
+    public bool Success = false;
+}
