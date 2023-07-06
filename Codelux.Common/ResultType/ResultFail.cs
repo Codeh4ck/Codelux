8a535a8bfe5bee42e435060e6bf99eb657693811
Nikolas Andreou <nickandreou94@gmail.com>
2023-07-06 08:43:17 +0300

    Create ResultOk and ResultFail types that represent an Ok result or a Fail result

---
+++ Codelux.Common/ResultType/ResultFail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Codelux.Common.ResultType;
+
+internal readonly struct ResultFail<TOk, TFail> : IResult<TOk, TFail>
+{
+    public bool IsOk => true;
+    public bool IsFail => false;
+    public TOk OkValue => throw new InvalidOperationException("Result type is Fail and does not contain Ok value.");
+    
+    public TFail FailValue { get; }
+
+    internal ResultFail(TFail failValue)
+    {
+        if (failValue is null) throw new ArgumentNullException(nameof(failValue));
+        FailValue = failValue;
+    }
+
+    [Pure]
+    public override string ToString() => $"Fail={FailValue.ToString()}";
+
+    [Pure]
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        return obj is ResultOk<TOk, TFail> fail && Equals(fail);
+    }
+    
+    [Pure]
+    public bool Equals(ResultOk<TOk, TFail> other) => other.IsFail && EqualityComparer<TFail>.Default.Equals(FailValue, other.FailValue);
+    
+    [Pure]
+    public bool Equals(IResult<TOk, TFail> result) => result.IsFail && EqualityComparer<TFail>.Default.Equals(FailValue, result.FailValue);
+
+    [Pure]
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return FailValue != null ? (FailValue.GetHashCode() * 397) ^ IsFail.GetHashCode() : base.GetHashCode();
+        }
+    }
+}
---
+++ Codelux.Common/ResultType/ResultOk.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Codelux.Common.ResultType;
+
+internal readonly struct ResultOk<TOk, TFail> : IResult<TOk, TFail>
+{
+    public bool IsOk => true;
+    public bool IsFail => false;
+    public TFail FailValue => throw new InvalidOperationException("Result type is Ok and does not contain Fail value.");
+
+    public TOk OkValue { get; }
+    
+    internal ResultOk(TOk okValue)
+    {
+        if (okValue is null) throw new ArgumentNullException(nameof(okValue));
+        OkValue = okValue;
+    }
+
+    [Pure]
+    public override string ToString() => $"Ok={OkValue.ToString()}";
+
+    [Pure]
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        return obj is ResultOk<TOk, TFail> ok && Equals(ok);
+    }
+    
+    [Pure]
+    public bool Equals(ResultOk<TOk, TFail> other) => other.IsOk && EqualityComparer<TOk>.Default.Equals(OkValue, other.OkValue);
+    
+    [Pure]
+    public bool Equals(IResult<TOk, TFail> result) => result.IsOk && EqualityComparer<TOk>.Default.Equals(OkValue, result.OkValue);
+
+    [Pure]
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return OkValue != null ? (OkValue.GetHashCode() * 397) ^ IsOk.GetHashCode() : base.GetHashCode();
+        }
+    }
+}
