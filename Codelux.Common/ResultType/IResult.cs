817618e7aad4fe520dfe52814b21684d95233c53
Nikolas Andreou <nickandreou94@gmail.com>
2023-07-06 08:42:46 +0300

    Create IResult base interface

---
+++ Codelux.Common/ResultType/IResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Codelux.Common.ResultType;
+
+public interface IResult<TOk, TFail>
+{
+    bool IsOk { get; }
+    bool IsFail { get; }
+    TOk OkValue { get; }
+    TFail FailValue { get; }
+    bool Equals(IResult<TOk, TFail> other);
+}
