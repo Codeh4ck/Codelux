c248894043f2134b263c8452d27737943342bcbd
Nikolas Andreou <nickandreou94@gmail.com>
2023-07-06 09:36:18 +0300

    Add Option type factory methods

---
+++ Codelux.Common/OptionType/OptionFactories.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Codelux.Common.OptionType;
+
+public static partial class Option
+{
+    public static Option<T> From<T>(T valueOrNull) => valueOrNull == null ? None<T>() : Some(valueOrNull);
+    public static Option<T> From<T>(T? valueOrNull) where T : struct => valueOrNull == null ? None<T>() : Some(valueOrNull.Value);
+    public static Option<T> Some<T>(T instance) => Option<T>.Some(instance);
+    public static Option<T> None<T>() => Option<T>.None();
+}
