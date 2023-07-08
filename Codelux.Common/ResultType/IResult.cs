a7b9c38486124fda9d4c2c2c490f609700e52e26
Nikolas Andreou <nickandreou94@gmail.com>
2023-07-08 16:00:15 +0300

    Create non-generic IResult type

--- Codelux.Common/ResultType/IResult.cs
+++ Codelux.Common/ResultType/IResult.cs
@@ -1,9 +1,13 @@
 ﻿namespace Codelux.Common.ResultType;
 
-public interface IResult<TOk, TFail>
+public interface IResult
 {
     bool IsOk { get; }
     bool IsFail { get; }
+}
+
+public interface IResult<TOk, TFail> : IResult
+{
     TOk OkValue { get; }
     TFail FailValue { get; }
     bool Equals(IResult<TOk, TFail> other);
