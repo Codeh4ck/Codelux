465bff532c55556ed99c7d3700c22c5cb5a0ebf6
Nikolas Andreou <nickandreou94@gmail.com>
2023-08-06 04:50:42 +0300

    Remove redundant parameter from Guard extensions

--- Codelux.Common/Extensions/ObjectExtensions.cs
+++ Codelux.Common/Extensions/ObjectExtensions.cs
@@ -5,14 +5,14 @@ namespace Codelux.Common.Extensions
 {
     public static class ObjectExtensions
     {
-        public static void Guard<T>(this T o, string message = "", [CallerArgumentExpression("o")] string paramName = "") where T : class
+        public static void Guard<T>(this T o, [CallerArgumentExpression("o")] string paramName = "") where T : class
         {
-            if (o == null) throw new ArgumentNullException(message, paramName);
+            if (o == null) throw new ArgumentNullException(paramName);
         }
 
-        public static void Guard(this string s, string message = "", [CallerArgumentExpression("s")] string paramName = "")
+        public static void Guard(this string s, [CallerArgumentExpression("s")] string paramName = "")
         {
-            if (string.IsNullOrEmpty(s)) throw new ArgumentNullException(message, paramName);
+            if (string.IsNullOrEmpty(s)) throw new ArgumentNullException(paramName);
         }
     }
 }
